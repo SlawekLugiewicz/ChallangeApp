cecfd20c7b1e2acc150949921f66f68f0694f7b9
SlawekLugiewicz <rehan8615@gmail.com>
2023-09-07 16:17:16 +0200

    Zadanie dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String name = "Ewa";
+char kobieta = 'f';
+var herAge =20;
+
+if (kobieta == 'f' && herAge < 33)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && herAge > 25)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (kobieta != 'f' && herAge < 18 )
+{
+    Console.WriteLine("Nastoletni mężczyzna");
+}
