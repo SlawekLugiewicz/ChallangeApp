20743d71f68dc90e433e54d75019471686b85b57
SlawekLugiewicz <rehan8615@gmail.com>
2024-03-01 11:48:50 +0100

    Zadanie dzień 8

---
+++ ChallangeApp/ChallangeApp.Test/TypeTest.cs
@@ -0,0 +1,53 @@
+﻿
+namespace ChallangeApp.Test
+{
+       
+    public class TypeTest
+    {
+        [Test]
+        public void WhenNameAewNotTheSame()
+        {
+            // arange 
+            string user1 = ("Slawek");
+            string user2 = ("Janusz");
+           
+            //act
+
+            // assert
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        [Test]
+        public void WhenNumnerAreNotTheSame1()
+        {
+            // arange 
+            int number1 = 2;
+            int number2 = 4;
+            //act
+
+            // assert
+            Assert.AreNotEqual(number1, number2);
+
+        }
+
+        [Test]
+        public void WhenNumberAreNotTheSmae2()
+        {
+            // arange 
+            float number3 = 2.5F;
+            float number4 = 4.5F;
+            //act
+
+            // assert
+            Assert.AreNotEqual(number3, number4);
+        }
+
+        private User GetUser(string name)
+        {
+            return new User(name);
+        }
+        
+
+        
+    }
+}
